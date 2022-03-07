e591e6ccbaad69be31121ebcbba80349db3f9f42
Guilherme Gonzalez <gcgonzalez99@hotmail.com>
2022-03-06 23:06:56 -0300

    feat: adicao do novo contexto na aplicacao

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -7,6 +7,7 @@ import { ThemeProvider } from '@mui/material/styles';
 import { createEmotionCache } from '../utils/create-emotion-cache';
 import { AuthProvider } from '../contexts/AuthContext'
 import { theme } from '../styles/theme';
+import { UsuarioProvider } from '../contexts/UsuarioContext';
 
 const clientSideEmotionCache = createEmotionCache();
 
@@ -17,24 +18,26 @@ const App = (props) => {
 
   return (
     <AuthProvider>
-      <CacheProvider value={emotionCache}>
-        <Head>
-          <title>
-            Orange Juice
-          </title>
-          <meta
-            name="viewport"
-            content="initial-scale=1, width=device-width"
-          />
-          <link rel="icon" type="image/x-icon" href="../../public/favicon.ico"></link>
-        </Head>
-        <LocalizationProvider dateAdapter={AdapterDateFns}>
-          <ThemeProvider theme={theme}>
-            <CssBaseline />
-            {getLayout(<Component {...pageProps} />)}
-          </ThemeProvider>
-        </LocalizationProvider>
-      </CacheProvider>
+      <UsuarioProvider>
+        <CacheProvider value={emotionCache}>
+          <Head>
+            <title>
+              Orange Juice
+            </title>
+            <meta
+              name="viewport"
+              content="initial-scale=1, width=device-width"
+            />
+            <link rel="icon" type="image/x-icon" href="../../public/favicon.ico"></link>
+          </Head>
+          <LocalizationProvider dateAdapter={AdapterDateFns}>
+            <ThemeProvider theme={theme}>
+              <CssBaseline />
+              {getLayout(<Component {...pageProps} />)}
+            </ThemeProvider>
+          </LocalizationProvider>
+        </CacheProvider>
+      </UsuarioProvider>
     </AuthProvider>
   );
 };
