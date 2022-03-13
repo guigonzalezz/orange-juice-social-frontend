71cb58761c67473d80319bd6055cfa057529420b
Guilherme Gonzalez <gcgonzalez99@hotmail.com>
2022-03-13 03:04:44 -0300

    chore: box do calendario separado para implementacao futura

---
+++ src/components/home/calendario.tsx
@@ -0,0 +1,39 @@
+import React, {useEffect, useState} from 'react'
+import { Box, Typography } from '@mui/material';
+
+
+
+interface IProps {
+  item: IEventos
+}
+interface IEventos {
+  titulo: string,
+  descricao: string,
+  data: string,
+  link: string
+}
+
+
+export const Calendario = (props) => {
+
+  const { info } = props
+  const [items, setItems] = useState(info)
+
+  useEffect(()=>{
+
+  },[])
+
+  return (
+    <Box sx={{
+      width: '100%',
+      height: 380,
+      display: 'flex',
+      alignItems: 'center',
+      justifyContent: 'center',
+      background: '#232323'
+   }}>
+     <Typography sx={{ color: '#454B50', fontSize: 24}}>Em construção...</Typography>
+   </Box>
+  );
+};
+
