9ace8ac9aa417bea80a82740c2313ce6bd090e89
Guilherme Gonzalez <gcgonzalez99@hotmail.com>
2022-03-19 19:14:33 -0300

    refactor: reorganizando a head das paginas

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -40,51 +40,13 @@ class CustomDocument extends Document {
     return (
       <Html lang="en">
         <Head>
-          <link
-            rel="preconnect"
-            href="https://fonts.googleapis.com"
-          />
-          <link
-            rel="preconnect"
-            href="https://fonts.gstatic.com"
-          />
-          <link
-            rel="stylesheet"
-            href="https://fonts.googleapis.com/css2?family=Inter:wght@100;200;300;400;500;600;700;800;900&display=swap"
-          />
-          <link
-            rel="stylesheet"
-            href="https://fonts.googleapis.com/css?family=Roboto+Mono|Roboto+Slab|Roboto:300,400,500,700&display=optional"
-          />
-          <link
-            rel="apple-touch-icon"
-            sizes="180x180"
-            href="/apple-touch-icon.png"
-          />
-          <link
-            rel="icon"
-            href="/favicon.ico"
-          />
-          <link
-            rel="icon"
-            type="image/png"
-            sizes="32x32"
-            href="/favicon-32x32.png"
-          />
-          <link
-            rel="icon"
-            type="image/png"
-            sizes="16x16"
-            href="/favicon-16x16.png"
-          />
-
+          <link href="https://fonts.googleapis.com" rel="preconnect" />
+          <link href="https://fonts.gstatic.com" rel="preconnect" />
+          <link href="https://fonts.googleapis.com/css2?family=Inter:wght@100;200;300;400;500;600;700;800;900&display=swap" rel="stylesheet" />
+          <link href="https://fonts.googleapis.com/css?family=Roboto+Mono|Roboto+Slab|Roboto:300,400,500,700&display=optional" rel="stylesheet" />
           <link href="https://fonts.googleapis.com/css2?family=Anton&family=Roboto:wght@400;500;700&display=swap" rel="stylesheet"/>
           <link href="https://fonts.googleapis.com/css2?family=Anton&family=Poppins:wght@100;300;500;700&family=Roboto:wght@400;500;700&display=swap" rel="stylesheet"/>
-          <meta
-            name="theme-color"
-            content="#111827"
-          />
-
+          <meta name="theme-color" content="#111827"/>
         </Head>
         <body>
           <Main />
