c9c0865be24437d7ada72c3ca838c0933de6e394
Guilherme Gonzalez <gcgonzalez99@hotmail.com>
2022-03-20 17:24:46 -0300

    refactor: removendo o usuarioContext que nao estava sendo utilizado

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -7,7 +7,6 @@ import { ThemeProvider } from '@mui/material/styles';
 import { createEmotionCache } from '../utils/create-emotion-cache';
 import { AuthProvider } from '../contexts/AuthContext'
 import { theme } from '../styles/theme';
-import { UsuarioProvider } from '../contexts/UsuarioContext';
 
 const clientSideEmotionCache = createEmotionCache();
 
@@ -18,30 +17,28 @@ const App = (props) => {
 
   return (
     <AuthProvider>
-      <UsuarioProvider>
-        <CacheProvider value={emotionCache}>
-          <Head>
-            <title>
-              Orange Juice
-            </title>
-            <meta
-              name="viewport"
-              content="initial-scale=1, width=device-width"
-            />
-            <link
-              rel="icon"
-              type="image/x-icon"
-              href="../../public/favicon.ico"
-            ></link>
-          </Head>
-          <LocalizationProvider dateAdapter={AdapterDateFns}>
-            <ThemeProvider theme={theme}>
-              <CssBaseline />
-              {getLayout(<Component {...pageProps} />)}
-            </ThemeProvider>
-          </LocalizationProvider>
-        </CacheProvider>
-      </UsuarioProvider>
+      <CacheProvider value={emotionCache}>
+        <Head>
+          <title>
+            Orange Juice
+          </title>
+          <meta
+            name="viewport"
+            content="initial-scale=1, width=device-width"
+          />
+          <link
+            rel="icon"
+            type="image/x-icon"
+            href="../../public/favicon.ico"
+          ></link>
+        </Head>
+        <LocalizationProvider dateAdapter={AdapterDateFns}>
+          <ThemeProvider theme={theme}>
+            <CssBaseline />
+            {getLayout(<Component {...pageProps} />)}
+          </ThemeProvider>
+        </LocalizationProvider>
+      </CacheProvider>
     </AuthProvider>
   );
 };
